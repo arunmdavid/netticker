85511c386ebeaa7ea7c216d183e418366dd9038f
Swaminathan <r.swaminathan@outlook.com>
2013-06-04 23:11:25 +0530

    Arun Commit

--- gen/com/example/netticker/R.java
+++ gen/com/example/netticker/R.java
@@ -13,10 +13,21 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int editText1=0x7f060001;
+        public static final int gallery1=0x7f060002;
+        public static final int textView1=0x7f060000;
+        public static final int toggleButton1=0x7f060003;
+    }
+    public static final class layout {
+        public static final int netticker=0x7f030000;
+    }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int Enable_time=0x7f040001;
+        public static final int ToggleButton=0x7f040002;
+        public static final int app_name=0x7f040000;
     }
     public static final class style {
-        public static final int AppTheme=0x7f040000;
+        public static final int AppTheme=0x7f050000;
     }
 }
