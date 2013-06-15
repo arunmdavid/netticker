7ba280ead657c93739e98499600acffdc7e86ba5
Swaminathan <r.swaminathan@outlook.com>
2013-06-15 23:19:25 +0530

    New project files for 4.0.3

---
+++ src/com/example/netticker/Netticker.java
@@ -0,0 +1,22 @@
+package com.example.netticker;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Netticker extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_netticker);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.netticker, menu);
+		return true;
+	}
+
+}
